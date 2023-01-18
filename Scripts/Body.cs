4e5b8633d34ef6ac8957213044ddd81515c260ec
oh-jinsu <ohjinsu@codersproduct.com>
2023-01-19 00:17:39 +0900

    Implement the actor controls

--- Scripts/Body.cs
+++ Scripts/Body.cs
@@ -1,10 +1,59 @@
 using Godot;
-using System;
 
 public class Body : KinematicBody
 {
-    public override void _Ready()
+    [Export]
+    private float speed = 4f;
+
+    [Export]
+    private float sensitiveness = 4f;
+
+    private Vector3 direction = Vector3.Zero;
+
+    public override void _Input(InputEvent @event)
     {
-        
+        if (@event is InputEventScreenDrag drag)
+        {
+            var half = GetViewport().GetVisibleRect().Size.x * 0.5;
+
+            if (drag.Position.x < half)
+            {
+                MoveDirection(drag);
+            }
+
+            if (drag.Position.x > half)
+            {
+                MoveRotation(drag);
+            }
+        }
+
+        if (@event is InputEventScreenTouch && !@event.IsPressed())
+        {
+            direction = Vector3.Zero;
+        }
+    }
+
+    private void MoveDirection(InputEventScreenDrag drag)
+    {
+        var relative = new Vector3(drag.Relative.x, 0f, drag.Relative.y);
+
+        if (relative.Length() < sensitiveness)
+        {
+            return;
+        }
+
+        direction = relative.Normalized();
+    }
+
+    private void MoveRotation(InputEventScreenDrag drag)
+    {
+        RotationDegrees += new Vector3(0f, -1f * drag.Relative.x, 0f);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        var vector = direction.Rotated(Vector3.Up, Rotation.y) * speed * delta;
+
+        MoveAndCollide(vector);
     }
 }
---
+++ Scripts/Head.cs
@@ -0,0 +1,32 @@
+using Godot;
+
+public class Head : Camera
+{
+    [Export]
+    private float maxRotationDegreeX;
+
+    [Export]
+    private float minRotationDegreeX;
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventScreenDrag drag)
+        {
+            var half = GetViewport().GetVisibleRect().Size.x * 0.5;
+
+            if (drag.Position.x <= half)
+            {
+                return;
+            }
+
+            var next = RotationDegrees + new Vector3(-1f * drag.Relative.y, 0f, 0f);
+
+            if (next.x < minRotationDegreeX || next.x > maxRotationDegreeX)
+            {
+                return;
+            }
+
+            RotationDegrees = next;
+        }
+    }
+}
