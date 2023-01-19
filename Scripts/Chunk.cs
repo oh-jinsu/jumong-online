58b50f741ca5f0cf8b06a6bce3d1f280abe12d50
오진수 <ohjinsu@codersproduct.com>
2023-01-19 13:23:06 +0900

    Remove noise

--- Scripts/Chunk.cs
+++ Scripts/Chunk.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public class Chunk : MeshInstance
 {
@@ -13,43 +12,15 @@ public class Chunk : MeshInstance
     private int height = 64;
 
     [Export]
-    private int size = 16;
+    private int size = 1;
 
     private readonly SurfaceTool tool = new SurfaceTool();
-
-    private readonly RandomNumberGenerator random = new RandomNumberGenerator();
 
     public override void _Ready()
     {
         Mesh = CreateMesh();
 
         CreateTrimeshCollision();
-    }
-
-    private OpenSimplexNoise CreateNoise()
-    {
-        var noise = new OpenSimplexNoise();
-
-        random.Randomize();
-
-        noise.Seed = random.RandiRange(0, int.MaxValue);
-
-        noise.Octaves = 3;
-
-        noise.Period = 64f;
-
-        noise.Persistence = 0.5f;
-
-        noise.Lacunarity = 2f;
-
-        return noise;
-    }
-
-    private static Vector3 GetVertex(int x, int z, OpenSimplexNoise noise)
-    {
-        var y = noise.GetNoise2d(x, z) * 0.5f + 1.5f;
-
-        return new Vector3(x, (Mathf.Pow(y, 4f) - 1f), z);
     }
 
     private Mesh CreateMesh()
@@ -60,19 +31,19 @@ public class Chunk : MeshInstance
 
         var mesh = new ArrayMesh();
 
-        var noise = CreateNoise();
-
         for (int x = 0; x < width * size; x += size)
         {
             for (int z = 0; z < height * size; z += size)
             {
-                var vertexLT = GetVertex(x, z, noise);
+                var y = 0f;
 
-                var vertexRT = GetVertex(x + size, z, noise);
+                var vertexLT = new Vector3(x, y, z);
 
-                var vertexRB = GetVertex(x + size, z + size, noise);
+                var vertexRT = new Vector3(x + size, y, z);
 
-                var vertexLB = GetVertex(x, z + size, noise);
+                var vertexRB = new Vector3(x + size, y, z + size);
+
+                var vertexLB = new Vector3(x, y, z + size);
 
                 var uvLT = new Vector2(0f, 0f);
 
